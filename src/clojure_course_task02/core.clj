5f36e76c3cf3ce97a4d6f9c78f78d2298a0a095c
Fanis Kalimullin <fmkalimullin@gmail.com>
2013-03-25 23:12:04 +0400

    first commit

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,14 +1,22 @@
 (ns clojure-course-task02.core
   (:gen-class))
 
+(defn get-directories [path]
+  (filter #(.isDirectory %) (file-seq (clojure.java.io/file path))))
+
+(defn get-files [path]
+  (filter #(.isFile %) (.listFiles path)))
 
 (defn find-files [file-name path]
-  "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (->> path
+       get-directories
+       (pmap get-files)
+       flatten
+       (pmap #(.getName %))
+       (filter #(not (nil? (re-find (re-pattern file-name) %))))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
-
 (defn -main [file-name path]
   (if (or (nil? file-name)
           (nil? path))
